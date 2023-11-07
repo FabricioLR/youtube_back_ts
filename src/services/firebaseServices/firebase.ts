8d21685a8bc90656e56cd2807ff2fab40752d077
FabricioLR <fabriciolongodearaujo@gmail.com>
2023-11-07 16:49:58 -0300

    update env variables

--- src/services/firebaseServices/firebase.ts
+++ src/services/firebaseServices/firebase.ts
@@ -2,16 +2,16 @@ import * as admin from "firebase-admin"
 
 const Bucket = "teste-javascript-2062a.appspot.com"
 const serviceAccount = {
-    type: process.env.type as string,
-    project_id: process.env.project_id as string,
-    private_key_id: process.env.private_key_id as string,
-    private_key: (process.env.private_key  as string).replace(/\\n/g, '\n'),
-    client_email: process.env.client_email as string,
-    client_id: process.env.client_id as string,
-    auth_uri: process.env.auth_uri as string,
-    token_uri: process.env.token_uri as string,
-    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url as string,
-    client_x509_cert_url: process.env.client_x509_cert_url as string,
+    type: process.env.TYPE as string,
+    project_id: process.env.PROJECT_ID as string,
+    private_key_id: process.env.PRIVATE_KEY_ID as string,
+    private_key: (process.env.PRIVATE_KEY  as string).replace(/\\n/g, '\n'),
+    client_email: process.env.CLIENT_EMAIL as string,
+    client_id: process.env.CLIENT_ID as string,
+    auth_uri: process.env.AUTH_URI as string,
+    token_uri: process.env.TOKEN_URI as string,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL as string,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL as string,
 }
 
 admin.initializeApp({
